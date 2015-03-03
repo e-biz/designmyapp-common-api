17b0fe13c6b7eb79075d038d9f453ad9e79bbdb2
Loïc Ortola <loic.ortola@free.fr>
2015-03-03 14:32:18 +0100

    Update Builder.java

--- src/main/java/mobi/designmyapp/common/engine/builder/Builder.java
+++ src/main/java/mobi/designmyapp/common/engine/builder/Builder.java
@@ -1,9 +1,15 @@
 /*
-Copyright © 2014 by eBusiness Information
-All rights reserved. This source code or any portion thereof
-may not be reproduced or used in any manner whatsoever
-without the express written permission of eBusiness Information.
-*/
+ Copyright 2015 eBusiness Information
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package mobi.designmyapp.common.engine.builder;
 
 
@@ -12,7 +18,7 @@ import mobi.designmyapp.common.engine.model.Template;
 /**
  * Builder generic interface to build a specific template,
  * the interface provides a type.
- * Created by loic on 7/7/14.
+ * Created by Loïc Ortola on 7/7/14.
  */
 public interface Builder<T extends Template> {
 
