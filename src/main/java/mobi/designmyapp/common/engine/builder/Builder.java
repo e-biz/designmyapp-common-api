92dfce4df413ed2a681b3707a27c7741ad961319
prenaudat <prenaudat@excilys.com>
2015-09-11 17:55:58 +0200

    Added getBuildSteps method to Builder

--- src/main/java/mobi/designmyapp/common/engine/builder/Builder.java
+++ src/main/java/mobi/designmyapp/common/engine/builder/Builder.java
@@ -14,7 +14,7 @@ package mobi.designmyapp.common.engine.builder;
 
 
 import mobi.designmyapp.common.engine.model.Template;
-
+import java.util.List;
 /**
  * Generic builder interface to build a specific template,
  * the interface provides a type.
@@ -36,4 +36,14 @@ public interface Builder<T extends Template> {
   public static enum Type {
     ANDROID, IOS, CONTAINER
   }
+
+    /**
+     * This method declares the necessary build steps related to the current template build cycle.
+     * For instance, you could have the following build step keys: [ "init_files", "download_assets", "resize_assets", "pre_rendering" ].
+     * The build step keys will be exposed on the API.
+     * @param template the template for the build
+     * @return the list of build step keys
+     */
+    List<String> getBuildSteps(T template);
+
 }
